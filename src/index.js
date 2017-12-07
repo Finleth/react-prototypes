94091d726fdeceaa171a67dafe8822bb611e222d
Finleth <jeffrey@paufamily.com>
2017-12-06 17:56:59 -0800

    Created a component function in my index.js file and a user object and displayed stuff on the dom according tho the user object

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const user = {
+    name: 'Jeffrey Poa',
+    luckyNumber: luckyNumber(),
+};
+
+function luckyNumber(){
+    return Math.floor(Math.random()*1001);
+}
+
+function Greeting(props){
+    const {name, luckyNumber} = props.person;
+
+    return (
+        <div className="container">
+            <h1>Hello {name}</h1>
+            <h2 className="text-muted">{luckyNumber}</h2>
+        </div>
+    );
+}
+
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    <Greeting person={user}/>,
     document.getElementById('root')
 );
